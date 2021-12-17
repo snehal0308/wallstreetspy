b1c434e6209d08bac2c926cb60f8ea13c2557393
Snehal Adbol <smitaadbol01@gmail.com>
2021-12-17 21:26:29 +0530

    edited secret key for security reasons

--- project1/settings.py
+++ project1/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s9_^n$+=wg-3^j+m4=$(n1iecz=1npme3ocb)61r$ei*uss$%%'
+SECRET_KEY = 'django-insecure-s9_^n$+=wg-3^j+m4=$(n1iecz=1npme3ocb)61r$ei*uss$%%e'
+
+# edited secret key for security reasons 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
